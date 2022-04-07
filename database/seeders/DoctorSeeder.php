25701d7b90cb54fb0e7daddb3b932609bb67dbab
Malika <malikajuraeva98@gmail.com>
2022-04-07 17:51:18 +0500

    feat: create doctors seeder

---
+++ database/seeders/DoctorSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DoctorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('doctors')->insert([
+            'fullname' => "malika",
+            'phone' => "",//9979618
+            'experience' => Str::random(20),
+        ]);
+    }
+}
